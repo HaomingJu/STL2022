12a242421649941f4c59991744e20efaccb1b085
HaomingJu <juhaoming@trunk.tech>
2022-01-06 17:12:51 +0800

    整理type_traits.h, 补充对裸指针的特性萃取

--- type_traits.h
+++ type_traits.h
@@ -37,142 +37,39 @@ struct __is_integer {
 
 #define INSIDE_BASE_IS_INTEGER typedef __true_type _Integral;
 
-template <>
-struct __type_traits<int> {
-    INSIDE_BASE_TYPE_TRAITS
-};
+template <> struct __type_traits<int> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<short> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<long> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<unsigned int> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<unsigned short> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<unsigned long> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<long long> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<unsigned long long> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<float> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<double> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<long double> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<bool> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<char> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<unsigned char> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __type_traits<signed char> { INSIDE_BASE_TYPE_TRAITS };
 
-template <>
-struct __type_traits<short> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<long> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<unsigned int> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<unsigned short> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<unsigned long> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<long long> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<unsigned long long> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<float> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<double> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<long double> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<bool> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<char> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<unsigned char> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __type_traits<signed char> {
-    INSIDE_BASE_TYPE_TRAITS
-};
+/* 对于所有裸指针, 均具有trivial construct/copy/assign/destroy 函数*/
+template <typename T> struct __type_traits<T*> { INSIDE_BASE_TYPE_TRAITS };
 
 // 判断是否是整数
 
-template <>
-struct __is_integer<int> {
-    INSIDE_BASE_IS_INTEGER
-};
-
-template <>
-struct __is_integer<short> {
-    INSIDE_BASE_IS_INTEGER
-};
-
-template <>
-struct __is_integer<long> {
-    INSIDE_BASE_IS_INTEGER
-};
-
-template <>
-struct __is_integer<unsigned int> {
-    INSIDE_BASE_IS_INTEGER
-};
-
-template <>
-struct __is_integer<unsigned short> {
-    INSIDE_BASE_IS_INTEGER
-};
-
-template <>
-struct __is_integer<unsigned long> {
-    INSIDE_BASE_IS_INTEGER
-};
-
-template <>
-struct __is_integer<long long> {
-    INSIDE_BASE_IS_INTEGER
-};
-
-template <>
-struct __is_integer<unsigned long long> {
-    INSIDE_BASE_IS_INTEGER
-};
-
-template <>
-struct __is_integer<bool> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __is_integer<char> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __is_integer<unsigned char> {
-    INSIDE_BASE_TYPE_TRAITS
-};
-
-template <>
-struct __is_integer<signed char> {
-    INSIDE_BASE_TYPE_TRAITS
-};
+template <> struct __is_integer<int> { INSIDE_BASE_IS_INTEGER };
+template <> struct __is_integer<short> { INSIDE_BASE_IS_INTEGER };
+template <> struct __is_integer<long> { INSIDE_BASE_IS_INTEGER };
+template <> struct __is_integer<unsigned int> { INSIDE_BASE_IS_INTEGER };
+template <> struct __is_integer<unsigned short> { INSIDE_BASE_IS_INTEGER };
+template <> struct __is_integer<unsigned long> { INSIDE_BASE_IS_INTEGER };
+template <> struct __is_integer<long long> { INSIDE_BASE_IS_INTEGER };
+template <> struct __is_integer<unsigned long long> { INSIDE_BASE_IS_INTEGER };
+template <> struct __is_integer<bool> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __is_integer<char> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __is_integer<unsigned char> { INSIDE_BASE_TYPE_TRAITS };
+template <> struct __is_integer<signed char> { INSIDE_BASE_TYPE_TRAITS };
 
 _STL2022_NAMESPACE_END
 
